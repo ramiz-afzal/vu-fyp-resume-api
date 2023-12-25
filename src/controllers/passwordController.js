bea867bd44d6be2bea8104a0be73619ed9fc7fec
Ramiz Afzal <m.ramiz.afzal@gmail.com>
2023-12-26 02:48:30 +0500

    core application logic completed

--- src/controllers/passwordController.js
+++ src/controllers/passwordController.js
@@ -2,8 +2,10 @@ import userModel from '../models/userModel.js';
 import resetTokenModal from '../models/resetTokenModal.js';
 import jwtHelpers from '../utils/jwtHelpers.js';
 import { validationResult } from 'express-validator';
+import { CourierClient } from '@trycourier/courier';
 import dotenv from 'dotenv';
 dotenv.config();
+const courier = new CourierClient({ authorizationToken: process.env.COURIER_AUTH_TOKEN });
 const controller = {
 	passwordRequest: async (req, res) => {
 		try {
@@ -26,9 +28,25 @@ const controller = {
 			await resetTokenModal.saveToken(user.id, refreshToken);
 
 			// TODO send reset token via email to user
+			await courier.send({
+				message: {
+					to: {
+						email: email,
+					},
+					content: {
+						title: 'Rest Resume App Password',
+						body: `Your reset token is:\n\n\n${refreshToken}`,
+					},
+					routing: {
+						method: 'single',
+						channels: ['email'],
+					},
+				},
+			});
 
 			return res.status(200).send({ message: 'Reset token sent over email' });
 		} catch (error) {
+			console.log(error);
 			return res.status(500).send({ message: 'Something went wrong' });
 		}
 	},
